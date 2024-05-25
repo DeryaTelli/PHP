c764f8f72bd6fc2f15b4a13960849e812e7c9896
Mobilyst <2002derya2002@gmail.com>
2024-05-25 15:23:02 +0300

    first commit

---
+++ A1PhpInfo.php
@@ -0,0 +1,15 @@
+//Php: hypertext preprocessor sunucu tarafli programlamaya odaklanmis HTML icerisine gomulebilen bir betik dilidir (scripting language )
+
+//1994 yilinda kisisel web sitesini yonetmek icin Rasmus Lerdorf tarafindan tasarlandi
+//Perl betikleri ile kodlandi 
+/1995 yilinda Php/Fi (kisisel ana sayfa / form yorumlayici) ilk kez duyurdu 
+//Zeev suraski ve andi gutmans in projeye dahil olmasi ile birlikte 1997 yilinda php 
+//ayrimcisini bastan yazildi ve dilin ismi php: Hypertext Preprocessor olarak degistirildi 
+//1998 yilinin haziran ayinda PHP 3 resmi olarak duyuruldu 
+//1999 yilinda Zend Motorunu gelistirdiler 
+//2000 yilinda PHP 4 resmi olarak duyuruldu
+//2004 yilinda PHP 5 resmi olarak duyuruldu nesneye yonelik programlama ile guclendirilmis 
+//2015 yilinda php 7 
+//2020 php 8 
+//Son kararli surumu 8.0.9dir 
+
---
+++ A2PhpTags.php
@@ -0,0 +1,20 @@
+<!-- 
+    Php etiketleri 
+    Echo Print komutlari 
+    komut ayrimi 
+    aciklama satirlari 
+-->
+<h1>Derya Telli</h1>
+<?php 
+// tek satirda aciklama yapmak icin iki slash kullanilir 
+# veya kare isaretini kullanabilirz 
+/* daha fazla satirda 
+aciklama icin kullanilir */
+echo 'Merhaba Derya Telli';
+print ' Naber?';
+?>
+
+
+<h1><?php echo 'Selam '?></h1>
+<!-- yukaridai kodun aynisidir echonun kisa halidir sadece -->
+<h1><?= 'Selam'?></h1> 
